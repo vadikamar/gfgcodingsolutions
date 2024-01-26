80885c052e06b63c06f5d232f8fe4ead44d105fb
vadikamar <87433086+vadikamar@users.noreply.github.com>
2024-01-26 17:20:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Fractional Knapsack/fractional-knapsack.py
@@ -0,0 +1,60 @@
+#User function Template for python3
+
+class Item:
+    def __init__(self,val,w):
+        self.value = val
+        self.weight = w
+        
+class Solution:    
+    #Function to get the maximum total value in the knapsack.
+    def fractionalknapsack(self, W,arr,n):
+        
+        # code here
+        capacity=0
+        answer=0
+        arr.sort(key=lambda x:x.value / x.weight , reverse = True)
+        
+        for item in arr:
+            if capacity + item.weight <= W:
+                capacity += item.weight
+                answer += item.value
+                
+            elif capacity == W:
+                break
+            
+            else:
+                remain= W - capacity 
+                answer += remain * ( item.value / item.weight )
+                capacity+= remain
+                
+        return answer 
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+class Item:
+    def __init__(self,val,w):
+        self.value = val
+        self.weight = w
+        
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,W = map(int,input().strip().split())
+        info = list(map(int,input().strip().split()))
+        arr = [Item(0,0) for i in range(n)]
+        for i in range(n):
+            arr[i].value = info[2*i]
+            arr[i].weight = info[2*i+1]
+            
+        ob=Solution()
+        print("%.6f" %ob.fractionalknapsack(W,arr,n))
+
+# } Driver Code Ends
