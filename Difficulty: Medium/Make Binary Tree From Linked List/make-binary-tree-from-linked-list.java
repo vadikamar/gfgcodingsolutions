41531f511259e281307f01d5b4e83e338d3629b1
vadikamar <87433086+vadikamar@users.noreply.github.com>
2024-07-01 22:19:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Make Binary Tree From Linked List/make-binary-tree-from-linked-list.java
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+
+class Tree {
+    int data;
+    Tree left;
+    Tree right;
+
+    Tree(int d) {
+        data = d;
+        left = null;
+        right = null;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    public Tree convert(Node head, Tree ans) {
+        if(head==null) return null;
+        ans=new Tree(head.data);
+        head=head.next;
+        Queue<Tree> q = new LinkedList<>();
+        q.add(ans);
+        while(head!=null)
+        {
+            Tree cur=q.poll();
+            cur.left=new Tree(head.data);
+            q.add(cur.left);
+            head=head.next;
+            if(head!=null){
+                cur.right=new Tree(head.data);
+                q.add(cur.right);
+                head=head.next; 
+            }
+        }
+        return ans;
+    }
+}
+
+
+//{ Driver Code Starts.
+class GFG {
+    static Node lhead;
+    static Queue<Tree> queue = new LinkedList<Tree>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            Tree node = null;
+            lhead = null;
+            Node head = null;
+            while (!queue.isEmpty()) {
+                queue.poll();
+            }
+            int n = sc.nextInt();
+            if (n != 0) {
+                GFG llist = new GFG();
+                int a1 = sc.nextInt();
+                head = new Node(a1);
+                llist.addToTheLast(head);
+                lhead = head;
+                for (int i = 1; i < n; i++) {
+                    int a = sc.nextInt();
+                    llist.addToTheLast(new Node(a));
+                }
+            }
+            Solution g = new Solution();
+            Tree root = g.convert(lhead, node);
+            try {
+                levelOrder(root, n);
+            } catch (NullPointerException ex) {
+                System.out.println(-1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void levelOrder(Tree root, int n) {
+        queue.add(root);
+        while (!queue.isEmpty() && n-- > 0) {
+            Tree proot = queue.remove();
+            try {
+                queue.add(proot.left);
+                queue.add(proot.right);
+                System.out.print(proot.data + " ");
+            } catch (NullPointerException ex) {
+            }
+        }
+    }
+
+    public void addToTheLast(Node head) {
+        if (lhead == null)
+            lhead = head;
+        else {
+            Node temp = lhead;
+            while (temp.next != null) temp = temp.next;
+            temp.next = head;
+        }
+    }
+}
+// } Driver Code Ends
