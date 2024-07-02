f0f39120d35b2c31e0e611ecd221f81a74c7e5e7
vadikamar <87433086+vadikamar@users.noreply.github.com>
2024-07-03 00:46:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GFG {
+    Node head;
+    Node tail;
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int N = sc.nextInt();
+            GFG llist = new GFG();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            Solution ob = new Solution();
+            head = ob.removeAllDuplicates(llist.head);
+            llist.printList(head);
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public Node removeAllDuplicates(Node head) {
+        // code here
+        HashMap<Integer,Integer> h=new HashMap<>();
+        Node cur=head;
+        while(cur!=null){
+            h.put(cur.data,h.getOrDefault(cur.data,0)+1);
+            cur=cur.next;
+        }
+        Node ans=new Node(0),h1=ans;
+        cur=head;
+        while(cur!=null){
+            if(h.get(cur.data)==1)
+            {
+                h1.next=new Node(cur.data);
+                h1=h1.next;
+            }
+            cur=cur.next;
+        }
+        return ans.next;
+    }
+}
