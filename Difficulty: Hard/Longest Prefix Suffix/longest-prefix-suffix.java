9fda265a3e216cad978cc3ec28b61871faaac1e3
vadikamar <87433086+vadikamar@users.noreply.github.com>
2024-09-22 18:54:53 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.lps(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution {
+    int lps(String s) {
+        // code here
+        
+        int pie[]=new int [s.length()];
+        
+        int pre=0,suf=1;
+        
+        while(suf<s.length()){
+            if(s.charAt(pre)==s.charAt(suf)){
+                pie[suf]=pre+1;
+                pre++;
+                suf++;
+            }else{
+                if(pre==0){
+                    pie[suf]=0;
+                    suf++;
+                }else{
+                    pre=pie[pre-1];
+                }
+            }
+        }
+        
+        return pie[s.length()-1];
+        
+    }
+}
